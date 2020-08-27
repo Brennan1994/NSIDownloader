6dfe641ca09ef5d28a2d6dd79e65050c154caa73
bbeam <Brennan.B.Beam@usace.army.mil>
2020-08-27 17:52:22 -0500

    write out redirect url to console

--- NSIDownloader/NSIDownloader/Program.cs
+++ NSIDownloader/NSIDownloader/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace NSIDownloader
     {
         static void Main(string[] args)
         {
+            string HECNSIRedirectURL = "http://www.hec.usace.army.mil/fwlink/?linkid=1&type=string";
+            WebClient webClient = new WebClient();
+            string downloadToFolderPath = "C:\\";
+            string FIPS = "06";
+            string xmin = "0";
+            string xmax = "1";
+            string ymin = "1";
+            string ymax = "1";
+
+            System.Console.WriteLine(webClient.DownloadString(HECNSIRedirectURL));
+            System.Console.Read();
         }
     }
 }
